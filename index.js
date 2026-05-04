ee1f121c8dfaf8922c8dda47c6a45b2b8cdf1368
FANDIE Yombissé Michel <153207110+yombisse@users.noreply.github.com>
2026-05-04 10:58:41 +0000

    Refactor dotenv config and PORT assignment in index.js
    Moved dotenv configuration to the top and updated PORT assignment logic.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+require('dotenv').config(); // doit être EN PREMIER
+
 const app = require("./server");
-require('dotenv').config();
- 
 
-// Render fournit PORT via les variables d'environnement
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.PORT || process.env.PORT || 3000;
 
-app.listen(PORT,'0.0.0.0',  () => {
-  console.log(` Serveur démarré sur le port ${PORT}`);
-  console.log(` API disponible sur http://127.0.0.1:${PORT}/api`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+  console.log(`API disponible sur http://127.0.0.1:${PORT}/api`);
 });
 
 module.exports = app;
